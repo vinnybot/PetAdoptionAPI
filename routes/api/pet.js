f6a9fd7bb4dab873f5e0c51a176c5032c342b94d
Amel_Topalovic <97201738+AmelTopalovic@users.noreply.github.com>
2022-09-27 16:56:23 -0500

    finished rest api code along pt1 and pt2

--- routes/api/pet.js
+++ routes/api/pet.js
@@ -1,101 +1,108 @@
 const debug = require('debug')('app:routes:api:pet');
 const debugError = require('debug')('app:error');
+const { parse } = require('dotenv');
 const express = require('express');
 const { nanoid } = require('nanoid');
+const dbModule = require('../../database');
 
-
-
-const petsArray = [
-  { _id: '1', name: 'Fido', createdDate: new Date() },
-  { _id: '2', name: 'Watson', createdDate: new Date() },
-  { _id: '3', name: 'Loki', createdDate: new Date() },
-];
+// const petsArray = [
+//   { _id: '1', name: 'Fido', createdDate: new Date() },
+//   { _id: '2', name: 'Watson', createdDate: new Date() },
+//   { _id: '3', name: 'Loki', createdDate: new Date() },
+// ];
 
 //create a router
 const router = express.Router();
 
 //define routes
-router.get('/api/pet/list', (req, res, next) => {
-  res.json(petsArray);
+router.get('/api/pet/list', async (req, res, next) => {
+  try {
+    const pets = await dbModule.findAllPets();
+    res.json(pets);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/api/pet/:petId', (req, res, next) => {
-  const petId = req.params.petId;
-
-  const foundPet = petsArray.find((x) => x._id == petId);
-  if (!foundPet) {
-    res.status(404).json({ error: 'Pet Not Found' });
-  } else {
-    res.json(foundPet);
+router.get('/api/pet/:petId', async (req, res, next) => {
+  try {
+    const petId = dbModule.newId(req.params.petId);
+    const pet = await dbModule.findPetById(petId);
+    if (!pet) {
+      res.status(404).json({ error: ` ${petId} Pet not found` });
+    } else {
+      res.json(pet);
+    }
+  } catch (err) {
+    next(err);
   }
 });
 //create
-router.put('/api/pet/new', (req, res, next) => {
-  const petId = nanoid();
-  const { species, name, gender } = req.body;
-  const age = parseInt(req.body.age);
-  
-  const pet = {
-    _id: petId,
-    species, //species:species,
-    name,
-    age,
-    gender,
-    createdDate:new Date(),
-  };
+router.put('/api/pet/new', async (req, res, next) => {
+  try {
+    const pet = {
+      _id: dbModule.newId(),
+      species: req.body.species,
+      name: req.body.name,
+      age: parseInt(req.body.age),
+      gender: req.body.gender,
+      createdDate: new Date(),
+
+    };
 
   //validation
-  if (!species) {
+  if (!pet.species) {
     res.status(400).json({ error: 'Species required' });
-  } else if (!name) {
+  } else if (!pet.name) {
     res.status(400).json({ error: 'Name required' });
-  } else if (!gender) {
-    res.status(400).json({ error: 'Gender required' });
-  } else if (!age) {
+  } else if (!pet.age) {
     res.status(400).json({ error: 'Age required' });
+  } else if (!pet.gender) {
+    res.status(400).json({ error: 'Gender required' });
   } else {
-    petsArray.push(pet);
-    res.json(pet);
+    await dbModule.insertOnePet(pet);
+    res.json({ message: 'Pet created' });
+  }
+  } catch (err) {
+    next(err);
   }
 });
 //update
-router.put('/api/pet/:petId', (req, res, next) => {
-  const petId = req.params.petId;
-  const { species, name, gender, age } = req.body;
+router.put('/api/pet/:petId', async (req, res, next) => {
+  try{
+    const petId = dbModule.newId(req.params.petId);
+    const update = req.body;
+    debug(`update pet ${petId}`, update);
 
-  const pet = petsArray.find((x) => x._id == petId);
-  if (!pet) {
-    res.status(404).json({ error: 'Pet Not Found' });
-  } else {
-    if (species != undefined) {
-      pet.species = species;
+    const pet = await dbModule.findPetById(petId);
+    if (!pet) {
+      res.status(404).json({ error: ` ${petId} Pet not found` });
+    } else {
+      await dbModule.updateOnePet(petId, update);
+      res.json({ message: ` ${petId} Pet updated` });
     }
-    if (name != undefined) {
-      pet.name = name;
-    }
-    if (age != undefined) {
-      pet.age = parseInt(age);
-    }
-    if (gender != undefined) {
-      pet.gender = gender;
-    }
-    pet.lastUpdated = new Date();
-    res.json(pet);
+  } catch (err) {
+    next(err);
   }
 });
 
 //delete
-router.delete('/api/pet/:petId', (req, res, next) => {
-  const petId = req.params.petId;
-  const index = petsArray.findIndex((x) => x._id == petId);
-  if (index < 0) {
-    res.status(404).json({ error: 'Pet Not Found' });
-  } else {
-    petsArray.splice(index, 1);
-    res.json({ message: 'Pet deleted' });
-  }
-});
+router.delete('/api/pet/:petId', async (req, res, next) => {
+ try{
+    const petId = dbModule.newId(req.params.petId);
+    debug(`delete pet ${petId}`);
 
+    const pet = await dbModule.findPetById(petId);
+    if (!pet) {
+      res.status(404).json({ error: ` ${petId} Pet not found` });
+    } else {
+      await dbModule.deleteOnePet(petId);
+      res.json({ message: ` ${petId} Pet deleted` });
+    }
+  } catch (err) {
+    next(err);
+ }
+});
 
 //export router
 module.exports = router;
